c3323c619998d8e8236f06fc69a73085f595c64f
punkyoon <cryptography@konkuk.ac.kr>
2017-09-07 01:52:05 +0900

    added a login checking

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,5 +4,6 @@ from accounts import views
 
 urlpatterns = [
     url(r'', include('django.contrib.auth.urls')),
-    url(r'register/', views.register_view, name='register'),
+    url(r'^register/', views.register_view, name='register'),
+    url(r'profile/', views.profile_view, name='profile'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
 def register_view(request):
     return render(request, 'registration/register.html')
+
+
+@login_required
+def profile_view(request):
+    return render(request, 'registration/profile.html')
