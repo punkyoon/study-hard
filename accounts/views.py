496b730b5d363a4a3b84c66eacfcf55c2c7d99ae
punkyoon <cryptography@konkuk.ac.kr>
2017-09-09 01:28:34 +0900

    added a register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+
+from accounts.models import Profile
 
 
 def register_view(request):
-    return render(request, 'registration/register.html')
+    error = None
+
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.email = request.POST['e-mail']
+            user.save()
+            
+            Profile.objects.create(
+                user=user,
+                gender=request.POST['gender'],
+                phone='+82'+request.POST['phone'],
+                institution=request.POST['institution']
+            )
+
+            login(request, user)
+            return redirect('/accounts/profile')
+    
+    error = 'Wrong ID or Password mismatch'
+    return render(request, 'registration/register.html', {'error': error})
 
 
 @login_required
