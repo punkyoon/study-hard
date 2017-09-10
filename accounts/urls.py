7d65ceb0028f6e841606d3aa869b548f691161ec
punkyoon <cryptography@konkuk.ac.kr>
2017-09-10 23:57:27 +0900

    modified html

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,5 +5,5 @@ from accounts import views
 urlpatterns = [
     url(r'', include('django.contrib.auth.urls')),
     url(r'^register/', views.register_view, name='register'),
-    url(r'profile/', views.profile_view, name='profile'),
+    url(r'^profile/', views.profile_view, name='profile'),
 ]
