d663fc7d7b1d5a918ebb6b7c1cc2f98a9b8e5f15
punkyoon <cryptography@konkuk.ac.kr>
2017-10-14 23:48:50 +0900

    fine->penalty

--- study_hard/settings.py
+++ study_hard/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'study_hard.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-'''
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -97,7 +97,7 @@ DATABASES = {
         'PORT': '5432',
     }
 }
-
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/1.11/ref/settings/#auth-password-validators
